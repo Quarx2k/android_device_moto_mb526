b711741121b04a81c2ff6945112f142e45e10d5f
Quarx2k <agent00791@gmail.com>
2012-08-02 00:18:20 +0700

    copy 2nd-init as 2nd-boot

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -36,7 +36,7 @@ cp -f $DEVICE_OUT/root/init $REPACK/ota/system/bootmenu/2nd-init/init
 # Use a prebuilt adbd configured for root access instead of normal one, for dev purpose
 cp -f $REPACK/ota/system/bootmenu/binary/adbd $REPACK/ota/system/bin/adbd
 
-cp -f $DEVICE_COMMON/bootmenu/binary/2nd-init $REPACK/ota/system/bootmenu/binary/2nd-init
+cp -f $REPACK/ota/system/bootmenu/binary/2nd-init $REPACK/ota/system/bootmenu/binary/2nd-boot
 
 # use the static busybox as bootmenu shell, and some static utilities
 cp -f $DEVICE_OUT/utilities/busybox $REPACK/ota/system/bootmenu/binary/busybox
